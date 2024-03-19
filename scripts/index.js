fda52edc04e03c614adcb1ab64be3590e5817e23
2Carrot10 <eleanorgrutz@gmail.com>
2024-03-19 10:08:56 -0700

    added color to links, and fixed empty input.

--- scripts/index.js
+++ scripts/index.js
@@ -5,9 +5,9 @@ var isMobile = function () {
     return check;
 }();
 
-/*if (isMobile) {
+if (isMobile) {
     window.location.replace("/mobile.html")
-}*/
+}
 
 
 var advancedMode = false;
@@ -29,90 +29,4 @@ export function expandContract() {
     else{
         document.getElementById("expand-contract-button").innerHTML="Advanced mode";
     }
-}
-
-const searchBox = document.getElementById("search-box");
-searchBox.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        simpleSearchFunction();
-    }
-});
-
-export function simpleSearchFunction() {
-    if (main.isSearchError()) return;
-    
-
-    main.deleteAllTiles();
-
-    var searchBoxValue = searchBox.value.toLowerCase();
-    var searchForPhrases = [];
-
-    //complecated regex to split string into string array based on spaces outside of quotes.
-    searchBoxValue.match(/(".*?"|[^" \s]+)(?=\s* |\s*$)/g).
-        forEach(
-            function (val, index) {
-                searchForPhrases[index] = val.replaceAll("\"", "")
-            }
-        );
-    for (var i = 0; i < searchForPhrases.length; i++) {
-        searchForPhrases[i] = searchForPhrases[i].trim();
-    }
-
-    main.getServiceData().sort(function (a, b) {
-        var aVal = getSimpleSearchRating(a, searchForPhrases);
-        var bVal = getSimpleSearchRating(b, searchForPhrases);
-        return -(aVal - bVal);
-    });
-
-    var hasFoundAnything=false;//purposfuly made global although this should be avoided.
-    //so lambda function can access this variable
-
-    var numberOfTiles = 0;
-
-    
-    for (let i = 0; i < main.getServiceData().length; i++) {
-        if (main.getServiceData()[i].searchRating > 0) {
-            main.renderOneTile(main.getServiceData()[i], searchForPhrases);
-            numberOfTiles++;
-        }
-    }
-    /*
-    hasFoundAnything=false;
-    (function myLoop(i) {
-        return setTimeout(function() {
-            if (main.getServiceData()[i].searchRating > 0) {
-                hasFoundAnything=true;
-            main.renderOneTile(main.getServiceData()[i], searchForPhrases); 
-            }
-          if (++i<main.getServiceData().length)myLoop(i,0);
-        }, 50)//ms wait between renders
-      })(0);  
-console.log("number of tiles="+hasFoundAnything)*/
-
-
-    if (numberOfTiles<1) {
-        main.renderNothingFoundCard();
-    }
-    document.documentElement.scrollTop = document.getElementById("groupDiv").offsetTop - 4;
-}
-
-
-//retune function
-function getSimpleSearchRating(opportunity, search) {
-    var value = 0;
-
-    //TODO: retune this
-    for (var i = 0; i < search.length; i++) {
-        value += 5 * countInstances(opportunity.description.toLowerCase(), search[i]);
-        value += 20 * countInstances(opportunity.title.toLowerCase(), search[i])
-        value += 6 * countInstances(opportunity.address.toLowerCase(), search[i])
-        value += 10 * countInstances(opportunity.website.toLowerCase(), search[i])
-        value += 30 * countInstances(opportunity.zipcode.toLowerCase(), search[i])
-    }
-    opportunity.searchRating = value;
-    return value;
-}
-
-function countInstances(string, word) {
-    return string.split(word).length - 1;
 }
